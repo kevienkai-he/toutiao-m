f353baf56e2eddbec3e07d2f7c294c0273baceac
kevienkai <hkqkcp@163.com>
2022-03-02 21:44:57 +0800

    项目初始化

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,20 @@
+// 之前的路由写在main中
+// 单独文件路由
+
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
+// 配置自己的路由
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+
 ]
 
+// 创建路由对象
 const router = new VueRouter({
   routes
 })
 
+// 默认导出路由
 export default router
